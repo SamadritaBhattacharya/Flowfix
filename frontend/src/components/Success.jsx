9bb749cb16584b5805f606a0e35692050a9135c0
Samadrita Bhattacharya <samadritabhatt163.official@gmail.com>
2024-03-26 14:43:54 +0530

    Updated

--- frontend/src/components/Success.jsx
+++ frontend/src/components/Success.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Success = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>Success</div>
+    <div>
+      <h1>
+        Your Complaint is registered successfully.
+      </h1>
+      <button onClick={() => navigate("/")} className='p-1 border-1 rounded-3xl bg-slate-300 w-20 ml-48  mt-2'>Go Back</button>  
+
+    </div>
   )
 }
 
