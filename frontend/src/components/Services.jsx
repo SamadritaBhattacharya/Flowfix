95583c67b49d27ad94101d00f7d6a91c103f5aaf
Arnabh <sinhaarnabh888@gmail.com>
2024-04-02 21:05:01 +0530

    fixes

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,63 +1,66 @@
 import React from 'react';
-import NavHead from './NavHead';
+import NavHead from './Navbar';
 
+import { GiArtificialIntelligence, GiMedicines, GiHealthPotion, GiHealthCapsule, GiHealthIncrease } from "react-icons/gi";
+import { GiMedicinePills } from "react-icons/gi";
 const services = [
   {
-    title: 'Leakage',
-    description: 'We build modern, responsive websites that are tailored to your specific needs.',
-    icon: 'fa-solid fa-code',
+    title: 'Health Monitoring',
+    description: 'Utilize wearable devices and AI algorithms to monitor vital signs, track health metrics, and provide real-time alerts for potential health issues.',
+    icon: <GiHealthPotion />,
   },
   {
-    title: 'Drainage',
-    description: 'Create a user-friendly mobile app that helps you connect with your audience.',
-    icon: 'fa-solid fa-mobile',
+    title: 'Diagnostic Imaging Analysis',
+    description: 'AI-powered analysis of medical imaging data (such as X-rays, MRIs, CT scans) for accurate diagnosis and detection of abnormalities',
+    icon: <GiMedicines />,
   },
   {
-    title: 'Clogged',
-    description: 'Improve your website\'s ranking and reach a wider audience through search engines.',
-    icon: 'fa-solid fa-search',
+    title: 'Predictive Analytics',
+    description: 'AI algorithms analyze patient data to predict disease progression, identify high-risk individuals, and recommend preventive interventions.',
+    icon: <GiArtificialIntelligence />,
   },
   {
-    title: 'Quick Services',
-    description: 'Improve your website\'s ranking and reach a wider audience through search engines.',
-    icon: 'fa-solid fa-search',
-  },
-  {
-    title: 'Quick Services',
-    description: 'Improve your website\'s ranking and reach a wider audience through search engines.',
-    icon: 'fa-solid fa-search',
+    title: 'Remote Patient Monitoring',
+    description: 'Continuous monitoring of patient health data remotely using AI-enabled devices, allowing early detection of health issues and proactive intervention.',
+    icon: <GiHealthIncrease />,
   },
   {
     title: 'Quick Services',
     description: 'Improve your website\'s ranking and reach a wider audience through search engines.',
-    icon: 'fa-solid fa-search',
+    icon: <GiHealthCapsule />,
   },
-  
- 
+  {
+    title: 'Virtual Health Assistants',
+    description: 'AI-driven virtual assistants provide personalized health guidance, medication management, and lifestyle recommendations through natural language interaction.',
+    icon: <GiMedicinePills />,
+  },
+
 ];
 
 const ServiceCard = ({ service }) => {
   return (
-    <div className="service-card bg-green-100 p-8 rounded-lg shadow-md hover:shadow-lg hover:scale-105 transition duration-300 mb-6 py-14
-    ">
-      <i className={`${service.icon} text-3xl text-green-700`}></i>
-      <h3 className="text-xl font-medium mt-4 mb-2">{service.title}</h3>
-      <p className="text-gray-700">{service.description}</p>
+    <div className="service-card bg-gray-500 p-8 rounded-lg shadow-md  hover:scale-105 hover:border-spacing-2 hover:shadow-[0px_9px_21px_5px_#c6f6d5]  transition duration-300 mb-6 py-14">
+      <div className="text-3xl text-gray-100 ml-36 lg:ml-44
+      ">{service.icon}</div>
+      <h3 className="text-xl text-gray-50 font-medium mt-4 mb-2">{service.title}</h3>
+      <p className="text-gray-200">{service.description}</p>
     </div>
   );
 };
 
 const Services = () => {
   return (
-    <section className="services px-4 py-16 text-center bg-gradient-to-r from-green-200 to-emerald-800 h-screen">
-        <NavHead/>
-      <h2 className="text-3xl font-bold mb-12 text-green-900 mt-10">Our Services</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {services.map((service) => (
-          <ServiceCard key={service.title} service={service} />
-        ))}
-      </div>
-    </section>
+    <>
+      <NavHead/>
+      <section className="services px-4 py-16 text-center bg-gradient-to-r  h-screen">
+        <h2 className="text-3xl font-bold mb-12 text-white -mt-11">OUR SERVICES</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 m-4 text-gray-700">
+          {services.map((service) => (
+            <ServiceCard key={service.title} service={service} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
