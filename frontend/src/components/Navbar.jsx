a57b66fd766b4f124a86d3fb2367255abc9ea108
Samadrita Bhattacharya <samadritabhatt163.official@gmail.com>
2024-04-02 21:03:46 +0530

    fixes:  Navbar and Hero page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,85 @@
-import React from 'react'
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link, NavLink, useMatch, useResolvedPath } from "react-router-dom";
 
-const Navbar = () => {
+export default function NavHead() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div>
-        <header>
-            <h2 id='logo' className='font-bold'>Water Watch</h2>
-            <nav className='navigation space-x-12'>
-                <a href = "a">Home</a>
-                <a href = "a">About</a> 
-                <a href = "a">Services</a>
-                <a href = "a">Contact</a>
-            </nav>
-        </header>
-    </div>
-  )
-}
+    <nav className="w-full mt-3 z-50 mb-4">
+      <div className=" mx-auto px-4 py-2 flex justify-between items-center">
+        {/* <div>
+          <img src="../logo.jpeg" alt="Logo" className="w-12 h-12 rounded-full" />
+        </div> */}
+        <div className="text-gray-200 flex text-3xl font-light">
+          <img
+            src="../flulogo.jpg"
+            alt="Logo"
+            className="w-12 h-12 rounded-full"
+          />
+          <Link to="/" className=" ml-6">
+            Fluxion
+          </Link>
+        </div>
 
-export default Navbar
+        {/* Hamburger menu icon */}
+        <div className="block md:hidden">
+          <button onClick={toggleMenu}>
+            <svg
+              className="w-8 h-8 text-gray-200"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
+
+        {/* Main navigation links */}
+        <ul
+          className={`md:flex md:space-x-16 text-lg  text-gray-50 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          <CustomLink
+            to="/services"
+            className="hover:text-green-900 hover:font-medium hover:rounded-full hover:bg-green-100 hover:p-1"
+          >
+            Services
+          </CustomLink>
+          <CustomLink
+            to="/maps"
+            className="hover:text-green-900 hover:font-medium hover:rounded-full hover:bg-green-100 hover:p-1"
+          >
+            Maps
+          </CustomLink>
+          <Link
+            to="/register"
+            className="hover:text-green-900 hover:font-medium hover:rounded-full hover:bg-green-100 hover:p-1"
+          >
+            Sign in / Register
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+
+  function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    );
+  }
+}
